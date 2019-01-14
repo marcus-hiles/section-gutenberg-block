c1da27c5aa7e2b75ebca93d96d36991df9325cce
Marcus Hiles <39972880+marcus-hiles@users.noreply.github.com>
2019-01-14 12:32:05 -0600

    Update paths.js

--- config/paths.js
+++ config/paths.js
@@ -5,10 +5,12 @@
  */
 
 const path = require( 'path' );
+
 const fs = require( 'fs' );
 
 // Make sure any symlinks in the project folder are resolved:
 const pluginDir = fs.realpathSync( process.cwd() );
+
 const resolvePlugin = relativePath => path.resolve( pluginDir, relativePath );
 
 // Config after eject: we're in ./config/
